5329829c9790651a1a06681c8678186788a759e6
Architolb <brandon_tolbert@outlook.com>
2022-06-27 08:35:14 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#greeting
+print("Hello and welcome to Nam-A-Baby!")
+#input of city they grew up in
+city = input("Now, which city did you grow up in?\n")
+#name of the first pet you ever had
+pet = input("Finally, what was the name of the first pet you ever had?\n")
+# combine the answers that were input and return the generated name
+print("Congratulations, your name will be " + city + " " + pet )
+
+
